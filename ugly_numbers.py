6af68082230bdbe73742e34a1b867ea61c27993c
Rishi Nanthan <rishinanthan344@gmail.com>
2020-11-10 19:12:34 +0530

    ugly number

--- ugly_numbers.py
+++ ugly_numbers.py
@@ -7,47 +7,47 @@
 
     ALGORITHM
 
-    initialize
-    
-    ugly[] =  | 1 |
-    i2 =  i3 = i5 = 0;
+        initialize
+        
+            ugly[] =  | 1 |
+            i2 =  i3 = i5 = 0;
 
-    First iteration
-    ugly[1] = Min(ugly[i2]*2, ugly[i3]*3, ugly[i5]*5)
-                = Min(2, 3, 5)
-                = 2
-    ugly[] =  | 1 | 2 |
-    i2 = 1,  i3 = i5 = 0  (i2 got incremented ) 
+        First iteration
+            ugly[1] = Min(ugly[i2]*2, ugly[i3]*3, ugly[i5]*5)
+                    = Min(2, 3, 5)
+                    = 2
+            ugly[] =  | 1 | 2 |
+            i2 = 1,  i3 = i5 = 0  (i2 got incremented ) 
 
-    Second iteration
-        ugly[2] = Min(ugly[i2]*2, ugly[i3]*3, ugly[i5]*5)
-                = Min(4, 3, 5)
-                = 3
-        ugly[] =  | 1 | 2 | 3 |
-        i2 = 1,  i3 =  1, i5 = 0  (i3 got incremented ) 
+        Second iteration
+            ugly[2] = Min(ugly[i2]*2, ugly[i3]*3, ugly[i5]*5)
+                    = Min(4, 3, 5)
+                    = 3
+            ugly[] =  | 1 | 2 | 3 |
+            i2 = 1,  i3 =  1, i5 = 0  (i3 got incremented ) 
 
-    Third iteration
-        ugly[3] = Min(ugly[i2]*2, ugly[i3]*3, ugly[i5]*5)
-                = Min(4, 6, 5)
-                = 4
-        ugly[] =  | 1 | 2 | 3 |  4 |
-        i2 = 2,  i3 =  1, i5 = 0  (i2 got incremented )
+        Third iteration
+            ugly[3] = Min(ugly[i2]*2, ugly[i3]*3, ugly[i5]*5)
+                    = Min(4, 6, 5)
+                    = 4
+            ugly[] =  | 1 | 2 | 3 |  4 |
+            i2 = 2,  i3 =  1, i5 = 0  (i2 got incremented )
 
-    Fourth iteration
-        ugly[4] = Min(ugly[i2]*2, ugly[i3]*3, ugly[i5]*5)
-                = Min(6, 6, 5)
-                = 5
-        ugly[] =  | 1 | 2 | 3 |  4 | 5 |
-        i2 = 2,  i3 =  1, i5 = 1  (i5 got incremented )
+        Fourth iteration
+            ugly[4] = Min(ugly[i2]*2, ugly[i3]*3, ugly[i5]*5)
+                    = Min(6, 6, 5)
+                    = 5
+            ugly[] =  | 1 | 2 | 3 |  4 | 5 |
+            i2 = 2,  i3 =  1, i5 = 1  (i5 got incremented )
 
-    Fifth iteration
-        ugly[4] = Min(ugly[i2]*2, ugly[i3]*3, ugly[i5]*5)
-                = Min(6, 6, 10)
-                = 6
-        ugly[] =  | 1 | 2 | 3 |  4 | 5 | 6 |
-        i2 = 3,  i3 =  2, i5 = 1  (i2 and i3 got incremented )
+        Fifth iteration
+            ugly[4] = Min(ugly[i2]*2, ugly[i3]*3, ugly[i5]*5)
+                    = Min(6, 6, 10)
+                    = 6
+            ugly[] =  | 1 | 2 | 3 |  4 | 5 | 6 |
+            i2 = 3,  i3 =  2, i5 = 1  (i2 and i3 got incremented )
 
-    Will continue same way till I < 150
+        Will continue same way till I < 150
 
 """
 
